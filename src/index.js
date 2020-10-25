2b074c406b77ab92420c86feffa6584cb8df2ad9
Mikhail-Kurilov <mvkurilov@mail.ru>
2020-10-26 00:18:03 +0400

    init: push what i have, not finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = str.split('');
+  let lengthArr = arr.length;
+  let lengthBracketsConfig = bracketsConfig.length;
+  for (let i = 0; i < lengthArr; i++) {
+    let openBracket = bracketsConfig[i][0];
+    let closeBracket = bracketsConfig[i][1];
+    let a = 0;
+    for (let j = 0; j < lengthBracketsConfig; j++) {
+      if (arr[j] === openBracket) {
+        a += 1;
+      } else if (arr[j] === closeBracket) {
+        a -= 1;
+      }
+      if (a < 0)
+        return false;
+      if (openBracket = closeBracket) {
+        a ++
+        if (a / 2 === 0) {
+          return true;
+        } 
+        return false;
+      }  
+    }
+    if (a > 0)
+      return false;
+  }
+  return true;
 }
+
